1a95ce7f03c40dc305b5a133423a1694de11d5e7
David Liu <yliu3743@uwo.ca>
2022-11-13 23:52:54 -0500

    simpsons documentation

--- R/simpsons.R
+++ R/simpsons.R
@@ -1,3 +1,14 @@
+#' Simpson's Rule
+#'
+#' @param f a function with a single argument x
+#' @param range a vector specifying the lower and upper end points of the range of integration (a and b)
+#' @param n the number of intervals for the Simpsons approximation
+#'
+#' @return a single value which is the Simpsons approximation to the integral
+#' @export
+#'
+#' @examples > simpsons(function(x){1/x},c(1,2),500)
+#' 0.6931472
 simpsons <- function(f,range,n){
   a <- range[1]
   b <-range[2]
