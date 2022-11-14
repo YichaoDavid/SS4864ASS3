fc4a11d2f3aa22d37a6768b31c574de0a3feeb76
David Liu <yliu3743@uwo.ca>
2022-11-13 23:50:31 -0500

    small mistakes simpsons

--- R/simpsons.R
+++ R/simpsons.R
@@ -3,6 +3,6 @@ simpsons <- function(f,range,n){
   b <-range[2]
   dx <- (b-a)/n
   x <- seq(a+dx,b,dx)
-  i <- sum(((f(x-dx)+4f((2x-dx)/2)+f(x))/6)*dx)
+  i <- sum(((f(x-dx)+4*f((2*x-dx)/2)+f(x))/6)*dx)
   return(i)
 }
