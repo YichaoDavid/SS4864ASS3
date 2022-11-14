997fd9e6b929700ccb24d7e6680a5fd99c6de729
David Liu <yliu3743@uwo.ca>
2022-11-13 23:49:08 -0500

    trapezoid rule documentation

--- R/trapezoid.R
+++ R/trapezoid.R
@@ -1,4 +1,15 @@
-traperoid <- function(f, range, n){
+#' Trapezoid Rule
+#'
+#' @param f a function with a single argument x
+#' @param range a vector specifying the lower and upper end points of the range of integration (a and b)
+#' @param n the number of intervals for the Trapezoid approximation
+#'
+#' @return a single value which is the Trapezoid approximation to the integral
+#' @export
+#'
+#' @examples > trapezoid(function(x){log(x)},c(1,2),200)
+#' 0.3862933
+trapezoid <- function(f, range, n){
   a <- range[1]
   b <- range[2]
   dx <- (b-a)/n
