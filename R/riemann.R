316930350a460f63ce4e50a94d262969c931f7ae
David Liu <yliu3743@uwo.ca>
2022-11-13 23:43:41 -0500

    documentation of riemann

--- R/riemann.R
+++ R/riemann.R
@@ -1,3 +1,16 @@
+#' Riemann integration
+#'
+#' @param f a function with a single argument x
+#' @param range  a vector specifying the lower and upper end points of the range of integration (a and b)
+#' @param n the number of intervals for the Riemann approximation
+#'
+#' @return a single value which is the Riemann approximation to the integral
+#' @export
+#'
+#' @examples
+#' > riemann(function(x){x^2}, c(1,2), 100)
+#' 2.34835
+
 riemann <- function(f, range, n) {
   a <- range[1]
   b <- range[2]
